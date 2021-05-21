3d281c54600368bd910baa641bc5c66d84c8f8a4
ergwer99 <janiier2000stiiven@gmail.com>
2021-05-21 17:14:09 -0500

    se agrego la vista horarios y reserva, se establecieron los colores

---
+++ app/src/main/java/com/example/proyecto_moviles/Horarios.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_moviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Horarios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horarios);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto_moviles/Reservas.java
@@ -0,0 +1,21 @@
+package com.example.proyecto_moviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Reservas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservas);
+    }
+
+
+
+
+
+}
